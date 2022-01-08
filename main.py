ae37644f41b64876a0b4a587f5a5dbac1d13c264
aIFzzf <845567595@qq.com>
2022-01-08 17:58:54 +0800

    test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
+import urllib.request
 from PyQt5 import QtWidgets
 from QuickInstall import Ui_Form
+
+url="http://42.194.249.12/MayaCode"
 
 class MyPyQT_Form(QtWidgets.QWidget,Ui_Form):
     def __init__(self):
@@ -9,7 +12,12 @@ class MyPyQT_Form(QtWidgets.QWidget,Ui_Form):
 
     #实现pushButton_click()函数，textEdit是我们放上去的文本框的id
     def pushButton_click(self):
-        self.textEdit.setText("你点击了按钮")
+        req = urllib.request.Request(url)
+        resp = urllib.request.urlopen(req)
+        data = resp.read().decode('utf-8')
+
+        print(data)
+        # self.textEdit.setText("你点击了按钮")
 
 
 if __name__ == '__main__':
