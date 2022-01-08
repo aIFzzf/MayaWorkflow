657c842f3842aa0ac0b9cc5dec1d8a3808d58e0f
aIFzzf <845567595@qq.com>
2022-01-08 17:25:49 +0800

    add UI

--- main.py
+++ main.py
@@ -1,44 +1,19 @@
+import sys
+from PyQt5 import QtWidgets
+from QuickInstall import Ui_Form
 
-import git
-from git import Repo
-import os
-def mkdir(path):
-    folder = os.path.exists(path)
+class MyPyQT_Form(QtWidgets.QWidget,Ui_Form):
+    def __init__(self):
+        super(MyPyQT_Form,self).__init__()
+        self.setupUi(self)
 
-    if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
-        os.makedirs(path)  # makedirs 创建文件时如果路径不存在会创建这个路径
-        Repo.clone_from(url="https://github.com/aIFzzf/MayaWorkflow.git", to_path=MayaCodePath)
-        print("---  new folder...  ---")
-        print( "---  OK  ---")
-
-    else:
-        g = git.cmd.Git(path)
-        g.pull()
-        print("---  There is this folder!  ---")
-
-path = os.path.abspath(os.path.dirname(__file__))
-MayaCodePath = path + '\\MayaCode'
-mkdir(MayaCodePath)
+    #实现pushButton_click()函数，textEdit是我们放上去的文本框的id
+    def pushButton_click(self):
+        self.textEdit.setText("你点击了按钮")
 
 
-
-
-
-
-
-# app = Flask(__name__)
-
-# @app.route('/',methods=['POST'])
-# def api_message2():
-#     if request.headers['Content-Type'] == 'application/json':
-#         if(repo.is_dirty()):
-#             git.Repo.clone_from(url="https://github.com/aIFzzf/MayaWorkflow.git",to_path ="../MayaCode")
-#         else:
-#             repo2 = git.Repo('../MayaCode')
-#             o = repo.remotes.origin
-#             o.pull()
-#         return 'Webhook notification received', 202
-#
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    my_pyqt_form = MyPyQT_Form()
+    my_pyqt_form.show()
+    sys.exit(app.exec_())
